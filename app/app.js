a5076119b4b83ca4f2462c91481d54cf6904dbc6
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2018-06-28 11:07:08 +0200

    theming

--- app/app.js
+++ app/app.js
@@ -30,19 +30,18 @@ config(function($routeProvider, $mdThemingProvider) {
 
   // Material theme
   $mdThemingProvider.theme('default')
-    .primaryPalette('teal', {
+    .primaryPalette('brown', {
       'default': '400',   // by default use shade 400 from the pink palette for primary intentions
       'hue-1': '100',     // use shade 100 for the <code>md-hue-1</code> class
       'hue-2': '600',     // use shade 600 for the <code>md-hue-2</code> class
       'hue-3': 'A100'     // use shade A100 for the <code>md-hue-3</code> class
     })
-    .accentPalette('blue', {
+    .accentPalette('teal', {
       'default': '300'
     })
-    .warnPalette('pink')
-    .backgroundPalette('blue-grey', {
-      'default': '100',
-      'hue-1': '50'
+    .warnPalette('orange')
+    .backgroundPalette('brown', {
+      'default': '50'
     })
 })
 
