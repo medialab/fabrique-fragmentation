b5e83f300f4e128f5062f5d3663924b38f5b95ac
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2018-06-28 19:26:40 +0200

    prototype of computing indexes

--- app/views/home.js
+++ app/views/home.js
@@ -21,16 +21,18 @@ angular.module('fabfrag.home', ['ngRoute'])
           for (projet_id in res.data) {
             console.log('PROJET', projet_id)
             var projet = res.data[projet_id]
-            $scope.items.push({
-              shortName: projet_id,
-              date: {
-                depot: 'à faire',
-                promulgation: 'à faire'
-              }
-            })
+
+            var projet_alignement_sum = 0
+            var projet_fragmentation_sum = {}
+            var projet_count = 0
+            var k
+
             var lecture_id
             for (lecture_id in projet) {
               var lecture = projet[lecture_id]
+              var lecture_alignement_sum = 0
+              var lecture_fragmentation_sum = {}
+              var lecture_count = 0
               var article_id
               for (article_id in lecture) {
                 var d = lecture[article_id]
@@ -38,8 +40,45 @@ angular.module('fabfrag.home', ['ngRoute'])
                 d.groups // LR: {nc:630, np:36}
                 d.inter_cosign // 123
                 d.sign_amend // List of lists of {id: 200, groupe: "LR"}
+                if (!isNaN(indexes.alignement)) {
+                  lecture_alignement_sum += indexes.alignement
+                  for (k in indexes.fragmentation) {
+                    if (lecture_fragmentation_sum[k] === undefined) {
+                      lecture_fragmentation_sum[k] = 0
+                    }
+                    lecture_fragmentation_sum[k] += indexes.fragmentation[k]
+                  }
+
+                  lecture_count++
+                }
               }
+              projet_alignement_sum += lecture_alignement_sum
+              for (k in lecture_fragmentation_sum) {
+                if (projet_fragmentation_sum[k] === undefined) {
+                  projet_fragmentation_sum[k] = 0
+                }
+                projet_fragmentation_sum[k] += lecture_fragmentation_sum[k]
+              }
+              projet_count += lecture_count
             }
+
+            var projet_alignement_mean = projet_alignement_sum / projet_count
+            var projet_fragmentation_mean = {}
+            for (k in projet_fragmentation_sum) {
+              projet_fragmentation_mean[k] = projet_fragmentation_sum[k] / projet_count
+            }
+
+
+            $scope.items.push({
+              shortName: projet_id,
+              date: {
+                depot: 'à faire',
+                promulgation: 'à faire'
+              },
+              alignement: projet_alignement_mean || 0,
+              fragmentation: projet_fragmentation_mean
+            })
+
           }
           $scope.loading = false
         })
@@ -60,13 +99,26 @@ angular.module('fabfrag.home', ['ngRoute'])
       // Cosignatures potential: if every pair of parlementaires consigned once (and only once)
       var cosignatures_potential = sum_of_parlementaires * (sum_of_parlementaires - 1)
 
+      var sum_of_potential_external_cosignatures = cosignatures_potential - sum_of_internal_cosignatures
+
       // Sum of cosignatures
       var sum_cosignatures = d.inter_cosign + sum_of_internal_cosignatures
 
-      // Modularity share for internal
-      var modularity_share_internal = (1/(4 * sum_cosignatures)) * (sum_of_internal_cosignatures - sum_of_potential_internal_cosignatures * Math.pow(sum_of_parlementaires - 1, 2) / (2 * sum_cosignatures))
+      var internal_density = sum_of_internal_cosignatures / sum_of_potential_internal_cosignatures
+      var external_density = d.inter_cosign / sum_of_potential_external_cosignatures
 
-      console.log(modularity_share_internal || 0)
+      var groups_fragmentation = {}
+      var group_id
+      for (group_id in d.groups) {
+        var g = d.groups[group_id]
+        var group_density = g.nc / (g.np * (g.np - 1))
+        groups_fragmentation[group_id] = Math.max(0, 1 - group_density)
+      }
+
+      return {
+        alignement: external_density,
+        fragmentation: groups_fragmentation
+      }
 
 
     }
