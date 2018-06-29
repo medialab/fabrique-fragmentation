3a26f402264f549c38f599e17f0542e16f3b5a26
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2018-06-29 11:40:27 +0200

    minor code cleaning

--- app/views/home.js
+++ app/views/home.js
@@ -10,31 +10,31 @@ angular.module('fabfrag.home', ['ngRoute'])
 }])
 
 .controller('HomeCtrl', function($scope, $timeout, $location, dataStore) {
-  	$scope.loading = true
+	$scope.loading = true
 
-    dataStore.getCosignData().then(function(data){
-      $timeout(function(){
-        $scope.indexes = dataStore.getIndexes()
+  dataStore.getCosignData().then(function(data){
+    $timeout(function(){
+      $scope.indexes = dataStore.getIndexes()
 
-        $scope.items = []
-        var projet_id
-        for (projet_id in data) {
-          var projet_index = $scope.indexes.projets[projet_id]
-          $scope.items.push({
-            id: projet_id,
-            shortName: projet_id,
-            date: {
-              depot: 'à faire',
-              promulgation: 'à faire'
-            },
-            alignement: projet_index.alignement,
-            amendements: projet_index.amendements,
-            fragmentation: projet_index.fragmentation
-          })
+      $scope.items = []
+      var projet_id
+      for (projet_id in data) {
+        var projet_index = $scope.indexes.projets[projet_id]
+        $scope.items.push({
+          id: projet_id,
+          shortName: projet_id,
+          date: {
+            depot: 'à faire',
+            promulgation: 'à faire'
+          },
+          alignement: projet_index.alignement,
+          amendements: projet_index.amendements,
+          fragmentation: projet_index.fragmentation
+        })
 
-        }
-        $scope.loading = false
-      })
+      }
+      $scope.loading = false
     })
+  })
 
 })
